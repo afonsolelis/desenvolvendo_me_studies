741b3d98799957238454aab03d1abe2fd6679d53
Afonso Lelis <afonsolelis@gmail.com>
2022-03-07 08:57:19 -0300

    change name of controller test

--- spec/controllers/searching_algorithms/linear_search_controller_spec.rb
+++ spec/controllers/searching_algorithms/linear_search_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe SearchingAlgorithms::LinearSearchController, type: :controller do
-  context 'POST' do
+  context 'POST Linear Search' do
     it "should return 'Element x is not present in array provided'" do
       params = {
         array: [10, 20, 80, 30, 60, 50, 110, 100, 130, 170],
